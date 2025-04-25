c13c842ff0d36d7262a1a06a0bf6c6fae4bb459e
Miha-Gov <miha.govekar@gmail.com>
2025-04-25 05:51:37 +0000

    Doal model Garden, pot /garden za dodajanje vrtov in prikaz seznama vrtov. Po prijavi lahko ustvarimo nove vrtove

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, login_required, current_user
 from database import db
 import bcrypt
 
@@ -10,7 +10,7 @@ db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from models import User
+from models import User, Garden
 
 with app.app_context():
     db.create_all()
@@ -31,7 +31,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and bcrypt.checkpw(password, user.password):
             login_user(user)
-            return redirect(url_for('home'))
+            return redirect(url_for('garden'))
         flash('Napačen email ali geslo.')
     return render_template('login.html')
 
@@ -49,6 +49,22 @@ def register():
             db.session.commit()
             return redirect(url_for('login'))
     return render_template('register.html')
+
+@app.route('/garden', methods=['GET', 'POST'])
+@login_required
+def garden():
+    if request.method == 'POST':
+        name = request.form['name']
+        size = float(request.form['size'])
+        sunlight = request.form['sunlight']
+        if name and size > 0:
+            new_garden = Garden(name=name, size=size, sunlight=sunlight, user_id=current_user.id)
+            db.session.add(new_garden)
+            db.session.commit()
+            return redirect(url_for('garden'))
+        flash('Neveljavni podatki.')
+    gardens = Garden.query.filter_by(user_id=current_user.id).all()
+    return render_template('garden.html', gardens=gardens)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -5,3 +5,11 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.LargeBinary(60), nullable=False)
+    gardens = db.relationship('Garden', backref='user', lazy=True)
+
+class Garden(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    size = db.Column(db.Float, nullable=False)
+    sunlight = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
